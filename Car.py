39446e7d23fd1d420403f44c4ba16a319ecaf186
Julian Herrdum <alu0101282749@ull.edu.es>
2018-11-17 21:20:47 +0000

    added new Map class and some methods to print the map

--- Car.py
+++ Car.py
@@ -1,6 +1,6 @@
 from heapq import *
-import math
 from math import sqrt
+import numpy
 
 
 class Node():
@@ -21,94 +21,158 @@ class Node():
     def __lt__(self, other):
         return self.f < other.f
 
-def heuristics(node, end_node, h):
-    if h == 0:
-        return sqrt((node.position[0] - end_node.position[0]) ** 2) + ((node.position[1] - end_node.position[1]) ** 2)
-    elif h == 1:
-        return (abs(node.position[0] - end_node.position[0])) + abs((node.position[1] - end_node.position[1]))
+class Map():
+    def __init__(self, map_size=(10,10), start=(0,0), end = (6,6), obstacles=0):
+        self.m_size = map_size
+        self.map = numpy.zeros(shape=map_size)
+        self.m_obstacles = obstacles
+        while
+
+    # overwrite str method so the map can be printet
+    def __str__(self):
+        return str(self.map)
+
+    def
 
 
-def astar(maze, start, end, h):
-    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
 
-    # Create start and end node
-    start_node = Node(None, start)
-    start_node.g = start_node.h = start_node.f = 0
-    end_node = Node(None, end)
-    end_node.g = end_node.h = end_node.f = 0
+class Nav(object):
+    """docstring for Map"""
 
-    # Initialize both open and closed list
-    open_list = []
-    closed_list = []
+    def __init__(self, ):
+        self.size = size
+        self.start = start
+        self.end = end
+        self.obstacles = obstacles
+        self.maze = \
+           [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+        self.path = []
 
-    # Add the start node
-    heappush(open_list,start_node)
+    def heuristics(node, end_node, h):
+        if h == 0:
+            return sqrt((node.position[0] - end_node.position[0]) ** 2) + (
+                        (node.position[1] - end_node.position[1]) ** 2)
+        elif h == 1:
+            return (abs(node.position[0] - end_node.position[0])) + abs((node.position[1] - end_node.position[1]))
 
-    # Loop until you find the end
-    while open_list:
+    def astar(maze, start, end, h):
+        """Returns a list of tuples as a path from the given start to the given end in the given maze"""
 
-        # Get the current node
-        current_node = heappop(open_list)
+        # Create start and end node
+        start_node = Node(None, start)
+        start_node.g = start_node.h = start_node.f = 0
+        end_node = Node(None, end)
+        end_node.g = end_node.h = end_node.f = 0
 
-        # Pop current off open list, add to closed list
-        closed_list.append(current_node)
+        # Initialize both open and closed list
+        open_list = []
+        closed_list = []
 
-        # Found the goal
-        if current_node == end_node:
-            path = []
-            current = current_node
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
-            return path[::-1] # Return reversed path
+        # Add the start node
+        heappush(open_list, start_node)
 
-        # Generate children
-        children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+        # Loop until you find the end
+        while open_list:
 
-            # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+            # Get the current node
+            current_node = heappop(open_list)
 
-            # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
-                continue
+            # Pop current off open list, add to closed list
+            closed_list.append(current_node)
 
-            # Make sure walkable terrain
-            if maze[node_position[0]][node_position[1]] != 0:
-                continue
+            # Found the goal
+            if current_node == end_node:
+                path = []
+                current = current_node
+                while current is not None:
+                    path.append(current.position)
+                    current = current.parent
+                return path[::-1]  # Return reversed path
 
-            # Create new node
-            new_node = Node(current_node, node_position)
+            # Generate children
+            children = []
+            for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1),
+                                 (1, 1)]:  # Adjacent squares
 
-            # Append
-            children.append(new_node)
+                # Get node position
+                node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
-        # Loop through children
-        for child in children:
-
-            # Child is on the closed list
-            for closed_child in closed_list:
-                if child == closed_child:
+                # Make sure within range
+                if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (
+                        len(maze[len(maze) - 1]) - 1) or node_position[1] < 0:
                     continue
 
-            # Create the g, h and f values
-            child.g = current_node.g + 1
-            child.h = heuristics(child, end_node, h)    # h: choose heuristic case
-            child.f = child.g + child.h
-
-            # Child is already in the open list
-            for open_node in open_list:
-                if child == open_node and child.g > open_node.g:
+                # Make sure walkable terrain
+                if maze[node_position[0]][node_position[1]] != 0:
                     continue
 
-            # Add the child to the open list
-            open_list.append(child)
+                # Create new node
+                new_node = Node(current_node, node_position)
+
+                # Append
+                children.append(new_node)
+
+            # Loop through children
+            for child in children:
+
+                # Child is on the closed list
+                for closed_child in closed_list:
+                    if child == closed_child:
+                        continue
+
+                # Create the g, h and f values
+                child.g = current_node.g + 1
+                child.h = heuristics(child, end_node, h)  # h: choose heuristic case
+                child.f = child.g + child.h
+
+                # Child is already in the open list
+                for open_node in open_list:
+                    if child == open_node and child.g > open_node.g:
+                        continue
+
+                # Add the child to the open list
+                open_list.append(child)
+
+class AutonmCar(object):
+    """docstring for AutonmCar"""
+    def __init__(self):
+        self.nav = None
+
+
+    def getUserInput(self):
+
+        print("Map dimensions m x n(e.g. 10x10):")
+        n = int(input(' type in n (width) value:'))
+        m = int(input(' type in m (bright) value:'))
+        ac_map_size = (n,m)
+        print(' Map will be {} x {}\n'.format(ac_map_size[0], ac_map_size[1]))
+        # for testing here
+        ac_map = Map(ac_map_size,(1,2),(5,5), 10)
+        print (ac_map)
+
+
 
 
 def main():
-    #Todo: print_map
+
+
+
+
+    car1 = AutonmCar()
+    car1.getUserInput()
+
+    #Todo: print_map:       ok
     #Todo: print way
-    #Todo: Userimput
+    #Todo: Userimput        ok
     #Todo: create map method
     maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
@@ -124,11 +188,13 @@ def main():
     start = (0, 0)
     end = (7, 6)
 
-    path = astar(maze, start, end, 1)
-    print("sortest-path:", path)
+    #path = astar(maze, start, end, 1)
+    #print("sortest-path:", path)
     # maze[0][0]= 1
-    print("\n")
-    print("map:\n", maze)
+    #print("\n")
+    #print("map:\n", maze)
+
+    a = AutonmCar()
 
 
 if __name__ == '__main__':
